b6e89ee7bf8d0aeea3f3fd35d6124827ff42b3db
jaegujaegu <103498622+jaegujaegu@users.noreply.github.com>
2022-06-03 22:32:04 +0900

    Update Ability.java

--- AL1S-new/src/AL1S/Ability.java
+++ AL1S-new/src/AL1S/Ability.java
@@ -40,7 +40,7 @@ public class Ability {
 
         void RailGunClamp(UnitHuman fight, UnitHuman.PlayerChara User) {
             if (fight.BodyArmor.equals("소형") && (User.statValue[1] - fight.defend) > 0)
-                fight.MaxHP = fight.MaxHP - (User.statValue[1] - fight.defend)*20;
+                fight.MaxHP = fight.MaxHP - (User.statValue[1] - fight.defend)*2;
             System.out.println("지면 강타 efftect : "+User.statList[1] +" "+User.statValue[1]+" 만큼의 대미지");
             System.out.println("소형에게만 효과. 적 방어력 1당 1 감소");
 
@@ -52,6 +52,11 @@ public class Ability {
             System.out.println("대형,소형 모두에게 효과. 적 방어력 1당 1 감소");
         }
     }
-
+    void RailGunBlaze(UnitHuman fight, UnitHuman.PlayerChara User){
+            if (fight.BodyArmor.equals("대형") && (User.statValue[1] - fight.defend) > 0)
+                fight.MaxHP = fight.MaxHP - (User.statValue[0] - fight.defend)*8;
+            System.out.println("빛이여! effect : "+User.statList[0] +" "+User.statValue[0]+" 만큼의 데미지");
+            System.out.println("대형에게만 효과. 적 방어력 1당 1 감소");
+        }
     
 }
